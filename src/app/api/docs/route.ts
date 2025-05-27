a8f774b344445fb43de43ad442cbcaca326bdf24
alotofhee <ekgmljeong@gmail.com>
2025-05-27 13:10:08 +0900

    #4 feat: swagger-ui-dist 사용으로 변경하여 적용

--- src/app/api/docs/route.ts
+++ src/app/api/docs/route.ts
@@ -1,7 +1,6 @@
-// app/api/docs/route.ts
-import { swaggerSpec } from "@/app/lib/swagger";
+import { getApiDocs } from "@/app/lib/swagger";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(swaggerSpec);
+  return NextResponse.json(getApiDocs);
 }
--- src/app/swagger/SwaggerPage.tsx
+++ src/app/swagger/SwaggerPage.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
+import { useEffect, useRef } from "react";
+import { SwaggerUIBundle } from "swagger-ui-dist";
+import "swagger-ui-dist/swagger-ui.css";
 
-export default function SwaggerPage() {
-  return (
-    <div style={{ height: "100vh" }}>
-      <SwaggerUI url="/api/docs" />
-    </div>
-  );
+type Props = {
+  spec: Record<string, unknown>;
+};
+export default function PageContent({ spec }: Props) {
+  const swaggerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!swaggerRef.current || !spec) return;
+    SwaggerUIBundle({
+      domNode: swaggerRef.current,
+      spec,
+    });
+  }, [spec]);
+
+  return <div className="swagger-ui-wrapper" ref={swaggerRef} />;
 }
